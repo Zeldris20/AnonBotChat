4f1a9a938ff991e9bdbb4dbbc8229f1b3ee35d47
Zeldris <38335453+christianclapson@users.noreply.github.com>
2023-08-21 18:10:25 +1000

    Add files via upload

---
+++ AnonChatForm/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AnonChatForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChatTextBox = new System.Windows.Forms.RichTextBox();
+            this.MessageInput = new System.Windows.Forms.TextBox();
+            this.SendMessage = new System.Windows.Forms.Button();
+            this.BotTypingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ChatTextBox
+            // 
+            this.ChatTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.ChatTextBox.ForeColor = System.Drawing.Color.White;
+            this.ChatTextBox.Location = new System.Drawing.Point(117, 2);
+            this.ChatTextBox.Name = "ChatTextBox";
+            this.ChatTextBox.Size = new System.Drawing.Size(557, 448);
+            this.ChatTextBox.TabIndex = 0;
+            this.ChatTextBox.Text = "";
+            this.ChatTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // MessageInput
+            // 
+            this.MessageInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.MessageInput.ForeColor = System.Drawing.Color.White;
+            this.MessageInput.Location = new System.Drawing.Point(273, 383);
+            this.MessageInput.Name = "MessageInput";
+            this.MessageInput.Size = new System.Drawing.Size(209, 20);
+            this.MessageInput.TabIndex = 1;
+            this.MessageInput.TextChanged += new System.EventHandler(this.MessageInput_TextChanged);
+            this.MessageInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MessageInput_KeyPress);
+            // 
+            // SendMessage
+            // 
+            this.SendMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.SendMessage.ForeColor = System.Drawing.Color.White;
+            this.SendMessage.Location = new System.Drawing.Point(488, 381);
+            this.SendMessage.Name = "SendMessage";
+            this.SendMessage.Size = new System.Drawing.Size(75, 23);
+            this.SendMessage.TabIndex = 2;
+            this.SendMessage.Text = "Send";
+            this.SendMessage.UseVisualStyleBackColor = false;
+            this.SendMessage.Click += new System.EventHandler(this.SendMessage_Click);
+            this.SendMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SendMessage_KeyPress);
+            // 
+            // BotTypingLabel
+            // 
+            this.BotTypingLabel.AutoSize = true;
+            this.BotTypingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.BotTypingLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.BotTypingLabel.Location = new System.Drawing.Point(342, 346);
+            this.BotTypingLabel.Name = "BotTypingLabel";
+            this.BotTypingLabel.Size = new System.Drawing.Size(88, 16);
+            this.BotTypingLabel.TabIndex = 3;
+            this.BotTypingLabel.Text = "Bot is typing...";
+            this.BotTypingLabel.Visible = false;
+            this.BotTypingLabel.Click += new System.EventHandler(this.BotTypingLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BotTypingLabel);
+            this.Controls.Add(this.SendMessage);
+            this.Controls.Add(this.MessageInput);
+            this.Controls.Add(this.ChatTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ChatTextBox;
+        private System.Windows.Forms.TextBox MessageInput;
+        private System.Windows.Forms.Button SendMessage;
+        private System.Windows.Forms.Label BotTypingLabel;
+    }
+}
+
---
+++ AnonChatForm/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnonChatForm
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> botResponses;
+        public Form1()
+        {
+            InitializeComponent();
+
+         
+            // Attatch the event handler to the KeyPress event of Message
+
+            // Initialise bot responses 
+            botResponses = new Dictionary<string, string>
+            {
+                { "hello", "Hi there! How can I assist you?" },
+                { "how are you", "I'm just a bot, but I'm here and ready to help!" },
+                { "name", "I'm ChatBot, your friendly assistant." },
+                { "joke", "Sure, here's one: Why don't scientists trust atoms? Because they make up everything!" },
+                { "help", "Of course, I'm here to help! What do you need assistance with?" },
+                { "goodbye", "Goodbye! If you have more questions, don't hesitate to come back." }
+                // Add more user messages and responses as needed
+            };
+        }
+
+            
+        private void ShowBotTypingIndicator()
+        {
+            BotTypingLabel.Visible = true;
+          
+
+            
+
+
+        }
+        
+       private void HideBotTypingIndicator()
+        {
+            BotTypingLabel.Visible = false;
+
+            
+        }
+
+       
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChatTextBox.Visible = false;
+            MessageInput.Focus();
+            
+        }
+      
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void SendMessage_Click(object sender, EventArgs e)
+
+        {
+
+            string userMessage = MessageInput.Text;
+
+
+            if (!string.IsNullOrWhiteSpace(userMessage))
+            {
+                DisplayUserMessage("You: " + userMessage);
+
+                BotTypingLabel.Visible = false;
+
+                ShowBotTypingIndicator();
+
+                // Simulate bot thinking with a delay 
+                await Task.Delay(1500); // delay for 1.5 seconds
+
+               
+
+                string botResponse = GetBotResponse(userMessage);
+                DisplayBotMessage("Bot: " + botResponse);
+
+                HideBotTypingIndicator();
+
+
+                MessageInput.Clear();
+            }
+        }
+               private string GetBotResponse(string userMessage)
+              {
+            foreach (var kvp in botResponses)
+            {
+                if (userMessage.ToLower().Contains(kvp.Key.ToLower()))
+                {
+                    return kvp.Value;
+                }
+
+            }
+
+                return "I'm not sure how to respond to that. Feel free to ask me something else!";
+        }
+
+        private void MessageInput_TextChanged(object sender, EventArgs e)
+        {
+            ChatTextBox.Visible = true;
+        }
+
+        private void SendMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void MessageInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                SendMessage.PerformClick();
+                e.Handled = true;
+            }
+        }
+
+        private void BotTypingLabel_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Bot_Paint(object sender, PaintEventArgs e)
+        {
+
+        
+        }
+    
+        private void DisplayUserMessage (string message)
+        {
+            // remove the last message bubble 
+            if (ChatTextBox.Controls.Count > 0)
+            {
+                ChatTextBox.Controls.RemoveAt(ChatTextBox.Controls.Count - 1);
+            }
+            Panel messageBubble = new Panel
+            {
+                AutoSize = true,
+                MaximumSize = new Size(200, 200), // Limit bubble width
+                Margin = new Padding(5, 5, 50, 5),
+                Padding = new Padding(10),
+                BackColor = Color.LightBlue,
+                BorderStyle = BorderStyle.FixedSingle,
+                Dock = DockStyle.Right // Align user messages to the right
+            };
+            Label messageLabel = new Label
+            {
+                Text = message,
+                AutoSize = true
+            };
+            messageBubble.Controls.Add(messageLabel);
+            ChatTextBox.Controls.Add(messageBubble);
+
+           
+        }
+        private void DisplayBotMessage(string message)
+        {
+            if (ChatTextBox.Controls.Count > 0)
+            {
+                ChatTextBox.Controls.RemoveAt(ChatTextBox.Controls.Count - 1);
+            }
+            Panel messageBubble = new Panel
+            {
+                AutoSize = true,
+                MaximumSize = new Size(200, 200), // Limit bubble width
+                Margin = new Padding(5, 50, 5, 5),
+                Padding = new Padding(10),
+                BackColor = Color.LightGreen,
+                BorderStyle = BorderStyle.FixedSingle,
+                
+                Dock = DockStyle.Left // Align bot messages to the left
+            };
+            Label messageLabel = new Label
+
+            {
+                Text = message,
+                AutoSize = true
+            };
+            messageBubble.Controls.Add (messageLabel);
+            ChatTextBox.Controls.Add(messageBubble);
+        
+        }
+
+    }
+  }
+
